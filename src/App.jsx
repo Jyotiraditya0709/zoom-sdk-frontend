11429cf962ab3376c7ebd7afb8268a7b2017d9f0
Jyotiraditya0709 <mishrajyotiraditya49@gmail.com>
2025-11-07 17:41:39 +0530

    22 oct 2nd

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ import MeetingCreator from "./components/MeetingCreator/MeetingCreator";
 
 import "./App.css";
 import DefaultPage from "./components/DefaultPage";
-// new 22 oct version
+
+
 const App = () => {
   return (
     <MeetingProvider>
